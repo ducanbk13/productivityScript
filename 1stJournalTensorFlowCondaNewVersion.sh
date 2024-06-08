e835976853c6f3cce48fd6851f574040f56d2484
Duc An <ducanbk13@gmail.com>
2024-06-09 01:14:50 +0200

    the running version of tensorflow

--- 1stJournalTensorFlowCondaNewVersion.sh
+++ 1stJournalTensorFlowCondaNewVersion.sh
@@ -27,12 +27,12 @@ installEnvironment(){
     # Name of the conda environment
     # and Python version
     # ------------------------
-    ENV_NAME="tensorflow2.16.1"
-    PYTHON_VERSION="3.11"
-    # CUDATOOLKIT_VERSION="11.2"
-    # CUDNN_VERSION="8.1"
-    # TENSORFLOW_VERSION="2.6.0"
-    # KERAS_VERSION="2.6.0"
+    ENV_NAME="tensorflow2.10.0"
+    PYTHON_VERSION="3.10"
+    CUDATOOLKIT_VERSION="11.2.0"
+    CUDNN_VERSION="8.1.0.77"
+    TENSORFLOW_VERSION="2.10.0"
+    KERAS_VERSION="2.10.0"
 
 
     # Check if conda is installed
@@ -56,12 +56,16 @@ installEnvironment(){
     # source "$(conda info --base)/etc/profile.d/conda.sh"
     conda activate $ENV_NAME || error_exit "Failed to activate conda environment"
 
-    # # ------------------------
-    # # Install cudatoolkit and 
-    # # cudnn
-    # # ------------------------
-    # echo "Installing cudatoolkit version '$CUDATOOLKIT_VERSION' and cudnn version '$CUDNN_VERSION'..."
-    # conda install -y -c conda-forge cudatoolkit=$CUDATOOLKIT_VERSION cudnn=$CUDNN_VERSION
+    # ------------------------
+    # Install cudatoolkit and 
+    # cudnn
+    # ------------------------
+    echo "Installing cudatoolkit version '$CUDATOOLKIT_VERSION' and cudnn version '$CUDNN_VERSION'..."
+    # conda install -y -c conda-forge cudatoolkit=$CUDATOOLKIT_VERSION cudnn=$CUDNN_VERSION || error_exit "Failed to install CUDA and CUDNN"
+    conda install -y conda-forge::cudatoolkit=$CUDATOOLKIT_VERSION || error_exit "Failed to install CUDA"
+    conda install -y conda-forge::cudnn=$CUDNN_VERSION  || error_exit "Failed to install cuda and CuDNN"
+    
+
 
     # ------------------------
     # Configure the system paths. 
@@ -76,7 +80,7 @@ installEnvironment(){
     # ------------------------
     echo "Upgrade pip and install tensorflow version '$TENSORFLOW_VERSION'..."
     pip install --upgrade pip
-    pip install tensorflow[and-cuda] || error_exit "Failed to install TensorFlow"
+    pip install tensorflow==$TENSORFLOW_VERSION || error_exit "Failed to install TensorFlow"
     # pip install keras==$KERAS_VERSION || error_exit "Failed to install keras"
 
     
@@ -88,9 +92,6 @@ installEnvironment(){
     conda install -y anaconda::pandas
     conda install -y conda-forge::joblib
     conda install -y pytables
-
-
-
 
 
 }
