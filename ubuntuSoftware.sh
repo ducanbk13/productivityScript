d370950442b449e3f021741476c9a3427bdba063
ducanbk13 <ducanbk13@gmail.com>
2025-07-19 19:47:39 +0200

    add firefox install

--- ubuntuSoftware.sh
+++ ubuntuSoftware.sh
@@ -6,17 +6,17 @@ installEnvironment(){
     # ------------------------
     # Stop script if any fault happends
     # ------------------------
-    set -e
+    # set -e
 
     # ------------------------
     # https://github.com/morrownr/rtl8852bu/blob/1.19.3/README.md
     # ------------------------
-    function error_exit {
-        echo "$1" 1>&2
-        echo "Press Enter to exit..."
-        read
-        exit 1
-    }
+    # function error_exit {
+    #     echo "$1" 1>&2
+    #     echo "Press Enter to exit..."
+    #     read
+    #     exit 1
+    # }
     cd ~/Downloads/
     # -------------------------
     # Install microsoft edge
@@ -36,20 +36,16 @@ installEnvironment(){
     # -------------------------
     # Install firefox
     # -------------------------
-    # sudo install -d -m 0755 /etc/apt/keyrings
-    # wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-    # echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null    
-    # echo '
-    # Package: *
-    # Pin: origin packages.mozilla.org
-    # Pin-Priority: 1000
-
-    # Package: firefox*
-    # Pin: release o=Ubuntu
-    # Pin-Priority: -1' | sudo tee /etc/apt/preferences.d/mozilla    
-    # sudo apt update && sudo apt remove firefox
-    # sudo snap remove firefox
-    # sudo apt install -y firefox
+    sudo install -d -m 0755 /etc/apt/keyrings
+    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+    gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+    echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+    echo '
+    Package: *
+    Pin: origin packages.mozilla.org
+    Pin-Priority: 1000
+    ' | sudo tee /etc/apt/preferences.d/mozilla
+    sudo apt-get update && sudo apt-get install firefox
 
 
     # -------------------------
@@ -78,6 +74,9 @@ installEnvironment(){
     # -------------------------
     sudo apt install gnome-clocks
     sudo apt install gnome-tweaks
+    # sudo apt install gnome-shell-extensions gnome-shell-extension-prefs chrome-gnome-shell
+    sudo apt install gnome-shell-extension-manager
+    sudo apt install gnome-weather
 
     # -------------------------
     # Install unikey
@@ -89,8 +88,8 @@ installEnvironment(){
     # -------------------------
     # Install vscode
     # -------------------------
-    # echo "Installing vscode ..."
-    # sudo snap install code --classic
+    echo "Installing vscode ..."
+    sudo snap install code --classic
 
     # -------------------------
     # Install grub=customizer
